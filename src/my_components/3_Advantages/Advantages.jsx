233168cb2666e91bee440ca042ebd11504ec3771
19Kushagra0 <19Kushagra0@gmail.com>
2026-01-16 19:59:35 +0530

    advantages class name changed

--- src/my_components/3_Advantages/Advantages.jsx
+++ src/my_components/3_Advantages/Advantages.jsx
@@ -4,8 +4,8 @@ import "@/my_components/3_Advantages/Advantages.css";
 export default function Advantages() {
   return (
     <div className="advantages">
-      <h2>Advantages</h2>
-      <p>Here are a few reasons why users love Anki.</p>
+      <h2 className="advantages-title">Advantages</h2>
+      <p className="advantages-description">Here are a few reasons why users love Anki.</p>
       <div className="advantages-boxs-container">
         <div className="advantages-boxes">
           <div className="advantages-box">
