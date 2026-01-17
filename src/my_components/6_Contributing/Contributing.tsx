de07e291e66f85b318b1cd9f076f00bcc9b7bf95
19Kushagra0 <19Kushagra0@gmail.com>
2026-01-17 08:50:43 +0530

    concept mobile image problem solved

--- src/my_components/6_Contributing/Contributing.tsx
+++ src/my_components/6_Contributing/Contributing.tsx
@@ -1,91 +1,87 @@
-import React from 'react'
-import "./Contributing.css"
+import React from "react";
+import "./Contributing.css";
 
 export default function Contributing() {
-    return (
-        <div className='contributing'>
-            <h2 className="anki-title">contributing</h2>
-            <div className="anki-description-container">
-                <p className="anki-description">
-                    Anki is a community project where anyone can make contributions to help improve everyone's experience.
-                </p>
-            </div>
-            <div className="contributing-boxes-container">
-                <div className="contributing-box">
-                    <div className="contributing-box-top">
-                        <h3>Open source</h3>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="#968CA8"
-                            className="contributing-box-top-image"
-                            strokeWidth={3}
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                            />
-                        </svg>
-                    </div>
-                    <div className="contributing-box-bottom">
-                        If you know how to code you could help maintain Anki or create new add-ons/features.
-                    </div>
-
-                </div>
-                <div className="contributing-box">
-                    <div className="contributing-box-top">
-                        <h3>Share decks </h3>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="#968CA8"
-                            className="contributing-box-top-image"
-                            strokeWidth={3}
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                            />
-                        </svg>
-                    </div>
-                    <div className="contributing-box-bottom">
-
-                        Sharing your deck can make it easier for others to start learning what you're studying.
-
-                    </div>
-
-                </div>
-                <div className="contributing-box">
-                    <div className="contributing-box-top">
-                        <h3>Translate anki </h3>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="#968CA8"
-                            className="contributing-box-top-image "
-                            strokeWidth={3}
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                            />
-                        </svg>
-                    </div>
-                    <div className="contributing-box-bottom">
-
-                        Help with the efforts to translate Anki so it can become more accessible to everyone.
-
-
-                    </div>
-
-                </div>
-            </div>
+  return (
+    <div className="contributing">
+      <h2 className="anki-title">contributing</h2>
+      <div className="anki-description-container">
+        <p className="anki-description">
+          Anki is a community project where anyone can make contributions to
+          help improve everyone's experience.
+        </p>
+      </div>
+      <div className="contributing-boxes-container">
+        <div className="contributing-box">
+          <div className="contributing-box-top">
+            <h3>Open source</h3>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="#968CA8"
+              className="contributing-box-top-image"
+              strokeWidth={3}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+              />
+            </svg>
+          </div>
+          <div className="contributing-box-bottom">
+            If you know how to code you could help maintain Anki or create new
+            add-ons/features.
+          </div>
         </div>
-    )
+        <div className="contributing-box">
+          <div className="contributing-box-top">
+            <h3>Share decks </h3>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="#968CA8"
+              className="contributing-box-top-image"
+              strokeWidth={3}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+              />
+            </svg>
+          </div>
+          <div className="contributing-box-bottom">
+            Sharing your deck can make it easier for others to start learning
+            what you're studying.
+          </div>
+        </div>
+        <div className="contributing-box">
+          <div className="contributing-box-top">
+            <h3>Translate anki </h3>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="#968CA8"
+              className="contributing-box-top-image "
+              strokeWidth={3}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+              />
+            </svg>
+          </div>
+          <div className="contributing-box-bottom">
+            Help with the efforts to translate Anki so it can become more
+            accessible to everyone.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/my_components/7_Downloads/Downloads.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Downloads.css'
+
+export default function Downloads() {
+    return (
+        <div className='downloads'>
+
+            <h2 className="anki-title">Downloads
+            </h2>
+            <div className="anki-description-container">
+                <p className="anki-description">
+                    Choose the correct download for your platform. For details, see the
+                    installation guide.
+                </p>
+            </div>
+        </div>
+    )
+}
